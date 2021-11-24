52726a6394ac86512cc3e50f8ddebc2e0a005988
Onur AĞA <onuraga@windowslive.com>
2021-11-24 12:29:56 +0300

    Add project files.

---
+++ 0/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace _0
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.timer3 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 60000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Interval = 1000;
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// timer3
+			// 
+			this.timer3.Interval = 10;
+			this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(68, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(37, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(68, 35);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 15);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "label2";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(68, 62);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(39, 15);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.Red;
+			this.label4.Location = new System.Drawing.Point(8, 9);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(44, 15);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Dakika";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.BackColor = System.Drawing.Color.Yellow;
+			this.label5.Location = new System.Drawing.Point(8, 35);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(43, 15);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "Saniye";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.DeepSkyBlue;
+			this.label6.Location = new System.Drawing.Point(8, 62);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(38, 15);
+			this.label6.TabIndex = 2;
+			this.label6.Text = "Salise";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(137, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(74, 24);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Başlat";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(137, 53);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(74, 24);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Durdur";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(215, 83);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Timer timer3;
+	}
+}
+
---
+++ 0/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		int dakika = 0, saniye = 1, salise = 1;
+
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+			if (saniye == 60)
+			{
+				saniye = 0;
+			}
+			saniye++;
+			label2.Text = saniye.ToString();
+			
+			
+
+		}
+
+		private void timer3_Tick(object sender, EventArgs e)
+		{
+			if (salise == 100)
+			{
+				salise = 0;
+			}
+			salise++;
+			label3.Text = salise.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			timer1.Start();timer2.Start();timer3.Start();
+		}
+
+		private void timer4_Tick(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer1.Stop();timer2.Stop();timer3.Stop();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			dakika++;
+			label1.Text = dakika.ToString();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label1.Text = "0"; label2.Text = "0"; label3.Text = "0";
+		}
+	}
+}
